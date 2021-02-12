891a2fba02b63d2f5485e37b2049c6ed94c8ca21
Karl Mabunga <karlmabunga@yahoo.com>
2021-02-12 09:50:18 -0800

    Created the createSession middleware in auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,9 +2,39 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  Promise.resolve(req.cookies.shortlyid)
+  .then(hash => {
+    if (!hash) {
+      throw Error('No cookie found')
+    }
+    return model.Sessions.get({hash})
+  })
+  .then(session => {
+    if (!session) {
+      throw Error('No session found')
+    }
+    return session;
+  })
+  .catch(err => {
+    return models.Sessions.create()
+    .then(results => {
+      return models.Sessions.get({id: results.insertId})
+    })
+    .then(session => {
+      res.cookie('shortlyid', session.hash)
+      return session
+    })
+  })
+  .then(session => {
+    req.session = session
+    next()
+  })
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
 
+module.exports.verifySession = (req, res, next) => {
+
+};
